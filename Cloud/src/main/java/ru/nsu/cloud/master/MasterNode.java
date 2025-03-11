f08cc42b98df1ffd572e9a4a493bad941a9c9811
andrew-kozubenko <a.kozubenko@g.nsu.ru>
2025-03-11 22:16:16 +0700

    Master methods + tests

--- Cloud/src/main/java/ru/nsu/cloud/master/MasterNode.java
+++ Cloud/src/main/java/ru/nsu/cloud/master/MasterNode.java
@@ -4,23 +4,50 @@ import ru.nsu.cloud.api.RemoteTask;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class MasterNode {
-    public static void main(String[] args) {
-        try (Socket socket = new Socket("localhost", 5000);
+    private final BlockingQueue<RemoteTask<?, ?>> taskQueue = new LinkedBlockingQueue<>();
+
+    public void submitTask(RemoteTask<?, ?> task) {
+        try {
+            taskQueue.put(task);
+            System.out.println("Task added to queue: " + task);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Failed to add task to queue: " + e.getMessage());
+        }
+    }
+
+    public RemoteTask<?, ?> getNextTask() {
+        try {
+            return taskQueue.take();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("Failed to retrieve task from queue: " + e.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * Отправляет задачу на Worker и получает результат
+     */
+    public <T, R> R sendTaskToWorker(RemoteTask<T, R> task, T inputData, String workerHost, int workerPort) {
+        try (Socket socket = new Socket(workerHost, workerPort);
              ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
              ObjectInputStream ois = new ObjectInputStream(socket.getInputStream())) {
 
-            RemoteTask<Integer, Integer> task = (x) -> x * 2; // удаленная функция
+            // Отправляем задачу
             oos.writeObject(task);
-            oos.writeInt(10); // отправляем данные
+            oos.writeObject(inputData);
             oos.flush();
 
-            int result = ois.readInt();
-            System.out.println("Result from worker: " + result);
-
-        } catch (IOException e) {
+            // Читаем результат от Worker
+            return (R) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
+            return null;
         }
     }
 }
